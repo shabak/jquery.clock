e01f4d64dfdd1b6d58379e044353bf4325b86498
shabak <nikolay.shabak@gmail.com>
2014-10-03 00:02:47 +0400

    Callback and stoping added to countdown

--- jquery.clock.js
+++ jquery.clock.js
@@ -29,7 +29,7 @@
         };
         return api;
     };
-    $.countdown = function (obj, opt) {
+    $.countdown = function (obj, opt, callback) {
         var d = opt.time.d, h = opt.time.h, m = opt.time.m, s = opt.time.s;
         function tick() {
             if(s === 0) {
@@ -49,7 +49,12 @@
                 s = 60;
             }
             s--;
-            var str = fd(d) + ' ' + f(h)+':'+f(m)+':'+f(s);
+            var str = '';
+            if (d === 0 && h === 0 && m === 0 && s === 0) {
+                callback();
+            } else {
+                str = fd(d) + ' ' + f(h)+':'+f(m)+':'+f(s);
+            }
             obj.html(str);
         }
         function f(n){return(n<10)?('0'+n):n;}
@@ -67,24 +72,26 @@
             return res;
         }        
         var api = {
-            tick: tick
+            tick: tick,
+            intervalId: null
         };
         return api;
     };    
     $.fn.clock = function (opt) {
         var time = opt.time ? opt.time : {h:-1,m:-1,s:-1};
-        if (time.h < 0 || time.m < 0 || time.s < 0) return;
-        if (time.h > 23 || time.m > 59 || time.s > 59) return;
+        if (time.h < 0 || time.m < 0 || time.s < 0 || time.h > 23 || time.m > 59 || time.s > 59) return;
         var theClock = $.clock(this, opt);
         theClock.tick();
         setInterval(theClock.tick, 1000);
     };
     $.fn.countdown = function (opt) {
         var time = opt.time ? opt.time : {h:-1,m:-1,s:-1};
-        if (time.d < 0 || time.h < 0 || time.m < 0 || time.s < 0) return;
-        if (time.h > 23 || time.m > 59 || time.s > 59) return;
-        var theCountdown = $.countdown(this, opt);
+        if (time.d < 0 || time.h < 0 || time.m < 0 || time.s < 0 || time.h > 23 || time.m > 59 || time.s > 59) return;
+        var theCountdown = $.countdown(this, opt, function() {
+            clearInterval(theCountdown.intervalId);
+            opt.callback();
+        });
         theCountdown.tick();
-        setInterval(theCountdown.tick, 1000);
+        theCountdown.intervalId = setInterval(theCountdown.tick, 1000);
     };
 }(jQuery));
