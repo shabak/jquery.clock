86657329cdda9d73ff3b58df5e06863488a40832
shabak <nikolay.shabak@gmail.com>
2014-10-02 23:07:15 +0400

    Countdown functionality added

--- jquery.clock.js
+++ jquery.clock.js
@@ -3,7 +3,7 @@
  * clock.js v0.0.1
  * jQuery Simple Clock Plugin - released under MIT License 
  * @author Shabak Nikolay <nikolay.shabak@gmail.com> 
- * https://github.com/shabak/jquery.clock
+ * https://github.com/shabak/clock
  * Copyright (c) 2014 
  */
 (function ($) {
@@ -28,6 +28,48 @@
             tick: tick
         };
         return api;
+    };
+    $.countdown = function (obj, opt) {
+        var d = opt.time.d, h = opt.time.h, m = opt.time.m, s = opt.time.s;
+        function tick() {
+            if(s === 0) {
+                if (m === 0) {
+                    if (h === 0) { 
+                        if (d > 0) {
+                            d--;
+                        }
+                        h = 23;
+                    } else {
+                        h--;
+                    }
+                    m = 59;
+                } else {
+                    m--;
+                }
+                s = 60;
+            }
+            s--;
+            var str = fd(d) + ' ' + f(h)+':'+f(m)+':'+f(s);
+            obj.html(str);
+        }
+        function f(n){return(n<10)?('0'+n):n;}
+        function fs(d){
+            if (d === 1) return "день";
+            if (d === 2 || d === 3 || d === 4) return "дня";
+            return "дней";
+        }
+        function fd(d){
+            if (d === 0) return '';
+            var res = d + ' ';
+            if (d >= 0 && d <= 20) res += fs(d);
+            if (d >= 21 && d <= 100) res += fs(d % 10);
+            if (d >= 101) res += fs((d % 100) % 10);
+            return res;
+        }        
+        var api = {
+            tick: tick
+        };
+        return api;
     };    
     $.fn.clock = function (opt) {
         var time = opt.time ? opt.time : {h:-1,m:-1,s:-1};
@@ -36,5 +78,13 @@
         var theClock = $.clock(this, opt);
         theClock.tick();
         setInterval(theClock.tick, 1000);
+    };
+    $.fn.countdown = function (opt) {
+        var time = opt.time ? opt.time : {h:-1,m:-1,s:-1};
+        if (time.d < 0 || time.h < 0 || time.m < 0 || time.s < 0) return;
+        if (time.h > 23 || time.m > 59 || time.s > 59) return;
+        var theCountdown = $.countdown(this, opt);
+        theCountdown.tick();
+        setInterval(theCountdown.tick, 1000);
     };
 }(jQuery));
